c545aa15bdf009f16dcc5af23bdfb633a26a57b5
ElizaKnapp <eknapp20@stuy.edu>
2020-11-21 13:50:42 -0500

    constructors and basic get functions and reciprocal done. Still one error in the constructor because RealNumbers must have doubles. I don't understand yet.

---
+++ RationalNumber.java
@@ -0,0 +1,36 @@
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  public RationalNumber (int nume, int deno) {
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    }
+    else if (deno < 0) {
+      numerator = nume * -1;
+      denominator = deno * -1;
+    }
+    else {
+      numerator = nume;
+      denominator = deno;
+    }
+  }
+
+  public double getValue() {
+    return (double)numerator / (double)denominator;
+  }
+
+  public int getNumerator() {
+    return numerator;
+  }
+
+  public int getDenominator() {
+    return denominator;
+  }
+
+  public RationalNumber reciprocal() {
+    RationalNumber recip = new RationalNumber(denominator, numerator);
+    return recip;
+  }
+
+}
